255b617813a2517add8ecd6abf7fd688328889c1
srivastavarahul442 <srivastavarahul442@gmail.com>
2026-05-22 13:57:12 +0530

    created connection api

--- src/routes/request.js
+++ src/routes/request.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/auth");
 const ConnectionRequest = require("../models/connectionRequest");
-const User = require("../models/user")
+const User = require("../models/user");
 
 const requestRouter = express.Router();
 
@@ -52,6 +52,44 @@ requestRouter.post(
       res
         .status(500)
         .json({ message: "Error creating Request", error: err.message });
+    }
+  },
+);
+
+requestRouter.post(
+  "/request/review/:status/:requestId",
+  userAuth,
+  async (req, res) => {
+    try {
+      const loggedInUser = req.user;
+      const { requestId, status } = req.params;
+
+      const allowedStatus = ["accepted", "rejected"];
+      if (!allowedStatus.includes(status)) {
+        return res.status(400).json({ message: "Status not allowed!!!" });
+      }
+
+      const connectionRequest = await ConnectionRequest.findOne({
+        _id: requestId,
+        toUserId: loggedInUser._id,
+        status: "interested",
+      });
+
+      if (!connectionRequest) {
+        return res
+          .status(404)
+          .json({ message: "Connaction request not found" });
+      }
+
+      connectionRequest.status = status;
+      const data = await connectionRequest.save();
+
+      res.status(200).json({
+        message: `${loggedInUser.firstName} ${status} the connection rqquest`,
+        data,
+      });
+    } catch (err) {
+      res.status(400).send("Error : " + err.message);
     }
   },
 );
---
+++ src/routes/user.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const { userAuth } = require("../middlewares/auth");
+const ConnectionRequest = require("../models/connectionRequest");
+const userRouter = express.Router();
+
+userRouter.get("/user/requests/received", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    const connectionRequests = await ConnectionRequest.find({
+      toUserId: loggedInUser._id,
+      status: "interested",
+    });
+
+    if (connectionRequests.length == 0) {
+      return res.status(200).json({ message: "Connection request not found" });
+    }
+    res.status(200).json({
+      message: "fatched all connections request",
+      data: connectionRequests,
+    });
+  } catch (err) {
+    res.status(400).send("Error : " + err.message);
+  }
+});
+
+module.exports = userRouter;
